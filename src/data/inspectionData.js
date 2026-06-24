231b5c303e7244605a7ade87f9e10058bddc8d4e
maxliao2026-design <61307295+bob1127@users.noreply.github.com>
2026-06-24 15:25:31 +0800

    ewrwrwr

---
+++ src/data/inspectionData.js
@@ -0,0 +1,315 @@
+// Google Drive: https://drive.google.com/drive/folders/11WNBAfIVMGbrq40BdR3BDZc9rc03n65N
+export const INSPECTION_DATA = [
+  {
+    "id": "peptides",
+    "label": "肽晶芙蓉檢驗",
+    "subCategories": [
+      {
+        "title": "台美西藥M61-260101310",
+        "items": [
+          {
+            "name": "台美西藥M61-260101310-肽晶芙蓉-1",
+            "url": "https://drive.google.com/file/d/1IyhqS4raWcV6EguAUOF-6CyYZGbRaXAc/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101310-肽晶芙蓉-2",
+            "url": "https://drive.google.com/file/d/11XbEyt72E1p-Iyk1gpBO_LB4MrCJOwoF/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101310-肽晶芙蓉-3",
+            "url": "https://drive.google.com/file/d/1stpv753rcfsoZSw1G7E5aSUIyaXct1o6/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101310-肽晶芙蓉-4",
+            "url": "https://drive.google.com/file/d/1-rg2mE6AuCcQcMK_Mp7RwEekN8SxqUMd/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101310-肽晶芙蓉-5",
+            "url": "https://drive.google.com/file/d/1_Z_KdykEst-sAaJFequULnkamLl_Nz9P/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101310-肽晶芙蓉-6",
+            "url": "https://drive.google.com/file/d/1rF2VqEjHULHCEgONCmiJ62N5ZNr7H8V-/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101310-肽晶芙蓉-7",
+            "url": "https://drive.google.com/file/d/19Mwl1_080A_yeIawjNnXf1DBbFM2QfWN/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101310-肽晶芙蓉-8",
+            "url": "https://drive.google.com/file/d/1bGIqLm2VHTscr2jOY-wzKbNYfDUXPUiE/view?usp=sharing"
+          }
+        ]
+      },
+      {
+        "title": "台美農殘M61-260101311",
+        "items": [
+          {
+            "name": "台美農殘M61-260101311-肽晶芙蓉-1",
+            "url": "https://drive.google.com/file/d/1yFfsP6Ouecqz1B4sNse8zUL_Goe3T8of/view?usp=sharing"
+          },
+          {
+            "name": "台美農殘M61-260101311-肽晶芙蓉-2",
+            "url": "https://drive.google.com/file/d/1LNhYcHFCcLwlL3bwBLcp5kaXZNjlXg0s/view?usp=sharing"
+          },
+          {
+            "name": "台美農殘M61-260101311-肽晶芙蓉-3",
+            "url": "https://drive.google.com/file/d/1AsBbHFN_gz_-5lk02nniHyEZwCMCzEWK/view?usp=sharing"
+          },
+          {
+            "name": "台美農殘M61-260101311-肽晶芙蓉-4",
+            "url": "https://drive.google.com/file/d/1d3K8dbcgeRhE2eBc37qVMAthjpW4pTjO/view?usp=sharing"
+          },
+          {
+            "name": "台美農殘M61-260101311-肽晶芙蓉-5",
+            "url": "https://drive.google.com/file/d/1mU3dK8fnhWFVzD7RSl_41VnSvsj-YbNG/view?usp=sharing"
+          },
+          {
+            "name": "台美農殘M61-260101311-肽晶芙蓉-6",
+            "url": "https://drive.google.com/file/d/1ArjMEPC5YZUuShMNTKeZ78wHzGSTIMa8/view?usp=sharing"
+          },
+          {
+            "name": "台美農殘M61-260101311-肽晶芙蓉-7",
+            "url": "https://drive.google.com/file/d/1TSqTx6CqEmd8WUseLxtUETPUAOnPKFeQ/view?usp=sharing"
+          }
+        ]
+      },
+      {
+        "title": "明嘉微生物M2512301",
+        "items": [
+          {
+            "name": "明嘉微生物M2512301-肽晶芙蓉",
+            "url": "https://drive.google.com/file/d/17BshKtJVMa7kUldMOKXw_sz0DAZOmd0s/view?usp=sharing"
+          }
+        ]
+      },
+      {
+        "title": "SGS種金屬PUF26100836",
+        "items": [
+          {
+            "name": "SGS種金屬PUF26100836-肽晶芙蓉-1",
+            "url": "https://drive.google.com/file/d/1ZNparlHrfVABOz90L62a45FL1FAPKM3p/view?usp=sharing"
+          },
+          {
+            "name": "SGS種金屬PUF26100836-肽晶芙蓉-2",
+            "url": "https://drive.google.com/file/d/1BYN8XennIKaqeTYJpfoRYDc8BPQY-FtY/view?usp=sharing"
+          },
+          {
+            "name": "SGS種金屬PUF26100836-肽晶芙蓉-3",
+            "url": "https://drive.google.com/file/d/1J_nsb-N0NV2B9h2Z7kaDl1OcctKOE_YJ/view?usp=sharing"
+          },
+          {
+            "name": "SGS種金屬PUF26100836-肽晶芙蓉-4",
+            "url": "https://drive.google.com/file/d/1BnFbLFXBli6mG_rPLs5JdwoVnEkVPfw7/view?usp=sharing"
+          }
+        ]
+      }
+    ],
+    "count": 20
+  },
+  {
+    "id": "gaba",
+    "label": "GABA鎂鎂香蜂草檢驗",
+    "subCategories": [
+      {
+        "title": "台美西藥M61-260101312",
+        "items": [
+          {
+            "name": "台美西藥M61-260101312-GABA鎂鎂香蜂草-1",
+            "url": "https://drive.google.com/file/d/1Ki5Q3j1Y7u4LzRkGKU1LRmg6Q6QQEHfb/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101312-GABA鎂鎂香蜂草-2",
+            "url": "https://drive.google.com/file/d/13vZNDnV_AdfMmiARnicWv-jIu18ecPSj/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101312-GABA鎂鎂香蜂草-3",
+            "url": "https://drive.google.com/file/d/1O56UrNfxECo72QmuEI7h3E873YgvXt_S/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101312-GABA鎂鎂香蜂草-4",
+            "url": "https://drive.google.com/file/d/1YzVqorCSA34xkl7NX1Uvz1GN1uBM41Kz/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101312-GABA鎂鎂香蜂草-5",
+            "url": "https://drive.google.com/file/d/1ztX7cXMTBenYJDMoj6pTcLwSbS5f97Bs/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101312-GABA鎂鎂香蜂草-6",
+            "url": "https://drive.google.com/file/d/1EZOn0zWhFWzYhUpLoDDUJIFDJUbcfQcb/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101312-GABA鎂鎂香蜂草-7",
+            "url": "https://drive.google.com/file/d/1kEFoBKHsVq5n1O6FZ1medmujozaMEAWC/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101312-GABA鎂鎂香蜂草-8",
+            "url": "https://drive.google.com/file/d/1G5V5L_kXEUb8A2jRBvfUee7Hyb3NOOew/view?usp=sharing"
+          }
+        ]
+      },
+      {
+        "title": "台美農殘M61-260101313",
+        "items": [
+          {
+            "name": "台美農殘M61-260101313-GABA鎂鎂香蜂草-1",
+            "url": "https://drive.google.com/file/d/1mJ_l4P90bdAbpRMagQAiO1MuwiNCG_US/view?usp=sharing"
+          },
+          {
+            "name": "台美農殘M61-260101313-GABA鎂鎂香蜂草-2",
+            "url": "https://drive.google.com/file/d/130FCXx5rygUsftqZgWtapzDfeU-7skXE/view?usp=sharing"
+          },
+          {
+            "name": "台美農殘M61-260101313-GABA鎂鎂香蜂草-3",
+            "url": "https://drive.google.com/file/d/1EtIM70cOTQYMt2nNgVisqTrUrLkZDjvQ/view?usp=sharing"
+          },
+          {
+            "name": "台美農殘M61-260101313-GABA鎂鎂香蜂草-4",
+            "url": "https://drive.google.com/file/d/1pBVB4DfqB6GqTovHy4Mb_ZaW9F_5ZjRj/view?usp=sharing"
+          },
+          {
+            "name": "台美農殘M61-260101313-GABA鎂鎂香蜂草-5",
+            "url": "https://drive.google.com/file/d/1bgr9iSRluxaiE1ns0Th4191Z4sOtRBIr/view?usp=sharing"
+          },
+          {
+            "name": "台美農殘M61-260101313-GABA鎂鎂香蜂草-6",
+            "url": "https://drive.google.com/file/d/1PMMduz-Sh0oNKAdPBKfMZbSOc6Zb-RVA/view?usp=sharing"
+          },
+          {
+            "name": "台美農殘M61-260101313-GABA鎂鎂香蜂草-7",
+            "url": "https://drive.google.com/file/d/1cm6nyMxgGOB5MUbGmSblYi2BHO-IgLvG/view?usp=sharing"
+          }
+        ]
+      },
+      {
+        "title": "明嘉微生物M2512272",
+        "items": [
+          {
+            "name": "明嘉微生物M2512272-GABA鎂鎂香蜂草",
+            "url": "https://drive.google.com/file/d/16olToGu57HcMfpLvNuNtGypQPbkUyshO/view?usp=sharing"
+          }
+        ]
+      },
+      {
+        "title": "SGS重金屬PUF26100837",
+        "items": [
+          {
+            "name": "SGS重金屬PUF26100837-GABA鎂鎂香蜂草-1",
+            "url": "https://drive.google.com/file/d/1YNqk4lAiuKUQ5sMt-bp0Z2vkGxqTPfmb/view?usp=sharing"
+          },
+          {
+            "name": "SGS重金屬PUF26100837-GABA鎂鎂香蜂草-2",
+            "url": "https://drive.google.com/file/d/1ERqW1tFiORP6hAqZzM43t36AnwKYUfl4/view?usp=sharing"
+          },
+          {
+            "name": "SGS重金屬PUF26100837-GABA鎂鎂香蜂草-3",
+            "url": "https://drive.google.com/file/d/1A3Pc7dJ88yeA_3tYjTBv7xGmVfNa7YCn/view?usp=sharing"
+          },
+          {
+            "name": "SGS重金屬PUF26100837-GABA鎂鎂香蜂草-4",
+            "url": "https://drive.google.com/file/d/16XYg-rozy_LOrR81yhb5bGUzDNbjoafE/view?usp=sharing"
+          }
+        ]
+      }
+    ],
+    "count": 20
+  },
+  {
+    "id": "probiotics",
+    "label": "維他菌合生元檢驗",
+    "subCategories": [
+      {
+        "title": "台美西藥M61-260101314",
+        "items": [
+          {
+            "name": "台美西藥M61-260101314-維他菌合生元-1",
+            "url": "https://drive.google.com/file/d/17iG8oN_zLJlRJLuXOnY4Azto6Iyu1Q_x/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101314-維他菌合生元-2",
+            "url": "https://drive.google.com/file/d/1Jp_trrvOhAeKyM3gbYE3f5Ee7k6OG-o6/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101314-維他菌合生元-3",
+            "url": "https://drive.google.com/file/d/1QHIT1orS8ml9e4hlP4k-SsUdy6GU4McN/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101314-維他菌合生元-4",
+            "url": "https://drive.google.com/file/d/1WeUzuzhYMSLhEoT9IodgS1_U8Hk4JYua/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101314-維他菌合生元-5",
+            "url": "https://drive.google.com/file/d/1uoGmu7IyCpeMRpmVqtdWuIX_-5gOAhIr/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101314-維他菌合生元-6",
+            "url": "https://drive.google.com/file/d/1tubpe6MNJ7MF0vtFpIXZ0sCKOwXKwfcK/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101314-維他菌合生元-7",
+            "url": "https://drive.google.com/file/d/1zJAS6Gtb0vEkSbwkKvtx7KYw2w-Fv8OW/view?usp=sharing"
+          },
+          {
+            "name": "台美西藥M61-260101314-維他菌合生元-8",
+            "url": "https://drive.google.com/file/d/1IFmjNMjV2DlihLkaH6e1pp5n6JcKj9_I/view?usp=sharing"
+          }
+        ]
+      },
+      {
+        "title": "台美農殘M61-260101315",
+        "items": [
+          {
+            "name": "台美農殘M61-260101315-維他菌合生元-1",
+            "url": "https://drive.google.com/file/d/1JmovhhVAqQJioSY_p4USxBdb6uF2_BkG/view?usp=sharing"
+          },
+          {
+            "name": "台美農殘M61-260101315-維他菌合生元-2",
+            "url": "https://drive.google.com/file/d/1bzHSKa_woQpCQy6Nl4zBvnsdYNBDR4IW/view?usp=sharing"
+          },
+          {
+            "name": "台美農殘M61-260101315-維他菌合生元-3",
+            "url": "https://drive.google.com/file/d/1DkD9ltEAn3QMie5CTLRufipxOEMaUWwI/view?usp=sharing"
+          },
+          {
+            "name": "台美農殘M61-260101315-維他菌合生元-4",
+            "url": "https://drive.google.com/file/d/1COIc__5mD_yVsTYJDJSvvTc1zHHgNpgX/view?usp=sharing"
+          },
+          {
+            "name": "台美農殘M61-260101315-維他菌合生元-5",
+            "url": "https://drive.google.com/file/d/1ev1aD21CD664Y5qkGmYE3M3L217-ZHTf/view?usp=sharing"
+          },
+          {
+            "name": "台美農殘M61-260101315-維他菌合生元-6",
+            "url": "https://drive.google.com/file/d/1IG1p0bmqWUVb4zdG7acSJYihzIWETxUn/view?usp=sharing"
+          },
+          {
+            "name": "台美農殘M61-260101315-維他菌合生元-7",
+            "url": "https://drive.google.com/file/d/12ROllKA5vmFlhWky8mrIURthOdrC6ufj/view?usp=sharing"
+          }
+        ]
+      },
+      {
+        "title": "SGS重金屬&微生物PUF26100838",
+        "items": [
+          {
+            "name": "SGS重金屬&微生物PUF26100838-維他菌合生元-1",
+            "url": "https://drive.google.com/file/d/1tk_GZWBAdbjjUTmt7VxlWzgcvmQ-grOX/view?usp=sharing"
+          },
+          {
+            "name": "SGS重金屬&微生物PUF26100838-維他菌合生元-2",
+            "url": "https://drive.google.com/file/d/1tgrO_V7xYxqpp3_ibtoDyuKtqOKY2Kt-/view?usp=sharing"
+          },
+          {
+            "name": "SGS重金屬&微生物PUF26100838-維他菌合生元-3",
+            "url": "https://drive.google.com/file/d/1VkWfOAbOo9eLlhyfc4RH76pqDMZmbczu/view?usp=sharing"
+          },
+          {
+            "name": "SGS重金屬&微生物PUF26100838-維他菌合生元-4",
+            "url": "https://drive.google.com/file/d/1vmeYtjveNybWIHXSKVB88XaIWP81sBwg/view?usp=sharing"
+          }
+        ]
+      }
+    ],
+    "count": 19
+  }
+];
